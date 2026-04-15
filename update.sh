37dae74e29f5baabfe6daaa935ff886cc5f44021
Kenji Saito <ken-yo@mbr.nifty.com>
2026-04-15 10:03:57 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -10,35 +10,34 @@ git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 
 cd "${CURRENT}" || exit
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 echo ""
 pwd
-npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm audit --fix && pnpm up -r && pnpm lint && pnpm build && pnpm install -r --no-frozen-lockfile
-result=$?
-if [ $result -ne 0 ]; then
+# if result=$(npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm audit --fix && pnpm up -r && pnpm lint && pnpm build && pnpm install -r --no-frozen-lockfile); then
+if result=$(npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint && pnpm build && pnpm install -r --no-frozen-lockfile); then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 
 cd "${CURRENT}" || exit
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 git pull --prune && git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 
 cd "${CUR}" || exit
